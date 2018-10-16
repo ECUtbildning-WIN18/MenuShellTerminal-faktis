e63d0f7302672063c7ba0aea7315ff1a64cbeda8
EdvinAhlgren <ahlgrenedvin@gmail.com>
2018-10-16 13:16:03 +0200

    Added xml with hashpassword

--- Domain/Database.cs
+++ Domain/Database.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Linq;
 using Domain;
 
 namespace Domain
@@ -8,10 +10,17 @@ namespace Domain
     {
         public static HashSet<string> UserNames { get; set; } = new HashSet<string>() { "Admin" };
         public static Dictionary<string, User> Users { get; set; } = new Dictionary<string, User>();
+        XDocument UsersXML = new XDocument();
         public Database()
         {
+            
             Users.Add("Admin", new SystemAdministrator("Admin", "PassWord"));
+            XElement User = new XElement("Users",
+                new XElement("Admin",
+                    new XElement("Name",Users["Admin"].UserName),
+                    new XElement("PassWord", Users["Admin"].SaltHashPassWord)));
+            UsersXML.Add(User);
+            UsersXML.Save("Users.xml");
         }
-        
     }
 }
--- Domain/User.cs
+++ Domain/User.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+//using System.Xml;
+using System.Xml.Linq;
 
 namespace Domain
 {
@@ -9,10 +13,20 @@ namespace Domain
         public string UserName { get; private set; }      
         public UserType UserType { get; private set; }
         private string passWord;
+        public string SaltHashPassWord { get; private set; }
         
         public User( string userName , string passWord , UserType userType)
         {
-            Init( userName , passWord , userType);
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+            var pbkdf2 = new Rfc2898DeriveBytes(passWord, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+            SaltHashPassWord = Convert.ToBase64String(hashBytes);
+            //DBContext.AddUser(new User { ..., Password = savedPasswordHash });
+            Init( userName , SaltHashPassWord, userType);
         }
 
         public void Init( string userName, string passWord, UserType userType)
@@ -24,7 +38,25 @@ namespace Domain
 
         public bool PassWordPass( string passWord )
         {
-            return this.passWord == passWord;
+            var reader = XDocument.Load("Users.xml");
+            var root = reader.Root;
+            var element = root.Elements();
+            var elementUser = element.ElementAt(0);
+            string savedPasswordHash = elementUser.Element("PassWord").Value;
+            /* Extract the bytes */
+            byte[] hashBytes = Convert.FromBase64String(savedPasswordHash);
+            /* Get the salt */
+            byte[] salt = new byte[16];
+            Array.Copy(hashBytes, 0, salt, 0, 16);
+            /* Compute the hash on the password the user entered */
+            var pbkdf2 = new Rfc2898DeriveBytes(passWord, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
+            /* Compare the results */
+            for (int i = 0; i < 20; i++)
+                if (hashBytes[i + 16] != hash[i])
+                    return false;
+            return true;
+            
         }
     }
 }
