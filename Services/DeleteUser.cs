643d693b571d211cb7d65aaea25f3bd1e8dde5a8
EdvinAhlgren <ahlgrenedvin@gmail.com>
2018-11-01 11:48:11 +0100

    add sql functionality to delete

--- Services/DeleteUser.cs
+++ Services/DeleteUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 using Domain;
 
@@ -9,8 +10,15 @@ namespace Services
     {
         public void Delete(string userName)
         {
-            Database.Users.Remove(userName);
-            Database.UserNames.Remove(userName);
+            var connectionString = "Data Source=(local);Initial Catalog=MenuShell; Integrated Security=true";
+            using (var connection = new System.Data.SqlClient.SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command= new SqlCommand($"DELETE FROM [User] WHERE UserName ='{userName}'", connection);
+                command.ExecuteNonQuery();
+                Database.Users.Remove(userName);
+                Database.UserNames.Remove(userName);
+            }
         }
     }
 }
